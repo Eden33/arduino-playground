95a514e2568bb1df33239a2b138f486a12bf6489
Eden33 <eduard.gopp@gmail.com>
2019-03-23 10:59:23 +0100

    use pin 2 to trigger ISR that resets the servo

--- timer-multi-tasking-v_1_1/timer-multi-tasking.ino
+++ timer-multi-tasking-v_1_1/timer-multi-tasking.ino
@@ -6,17 +6,25 @@
 Flasher2 led1(12, 2000, 2000);
 Flasher2 led2(13, 2000, 1000);
 Sweeper2 servo(50);
-static int servoStopPin = 2;
+static int servoResetPin = 2;
 
 void setup()
 {
   servo.Attach(10);
-  pinMode(servoStopPin, INPUT_PULLUP);
   
   // Timer0 is already used for millis() - we'll just interrupt somewhere
   // in the middle and call the "Compare A" function below
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
+
+  // set serverResetPin to input pull up and trigger ISR when the pin goes from high to low
+  pinMode(servoResetPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(servoResetPin), Reset, FALLING);
+}
+
+void Reset()
+{
+  servo.Reset();
 }
 
 // The timer interrupt is executed once a millisecond to update the LEDs and the servo.
@@ -24,12 +32,8 @@ void setup()
 SIGNAL(TIMER0_COMPA_vect) 
 {
   unsigned long currentMillis = millis();
-  
-  if(digitalRead(servoStopPin) == HIGH)
-  {
-     servo.Update(currentMillis);
-  }
-  
+    
+  servo.Update(currentMillis);
   led1.Update(currentMillis);
   led2.Update(currentMillis);
 }
